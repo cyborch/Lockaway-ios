a125e088ff7ce69c61bf4718e27449588f2ef4c6
Anders Borch <anders@cyborch.com>
2017-09-05 12:26:53 +0200

    Fix identifiers.

--- Lockaway/Discoverable.swift
+++ Lockaway/Discoverable.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2017 Anders Borch. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreBluetooth
 
 fileprivate let service = CBUUID(string: "EFC325B2-2281-4BB4-9196-8A8904980ABF")
-fileprivate let characteristic = CBUUID(string: "0E486070-4BDB-4902-9F78-C037F80B8577")
+fileprivate let socketCharacteristic = CBUUID(string: "0E486070-4BDB-4902-9F78-C037F80B8577")
+fileprivate let nameCharacteristic = CBUUID(string: "F021A685-9BC6-491B-A3DC-0816F0026F57")
 
 class Discoverable: NSObject, CBPeripheralManagerDelegate {
 
@@ -19,17 +20,25 @@ class Discoverable: NSObject, CBPeripheralManagerDelegate {
     
     @IBOutlet var controller: IntroController?
     
+    internal func addCharacteristics(manager: CBPeripheralManager) {
+        let primary = CBMutableService(type: service, primary: true)
+        let sock = CBMutableCharacteristic(type: socketCharacteristic,
+                                           properties: CBCharacteristicProperties.read,
+                                           value: nil,
+                                           permissions: CBAttributePermissions.readable)
+        let name = CBMutableCharacteristic(type: nameCharacteristic,
+                                           properties: CBCharacteristicProperties.read,
+                                           value: UIDevice.current.name.data(using: .utf8),
+                                           permissions: CBAttributePermissions.readable)
+        primary.characteristics = [sock, name]
+        manager.add(primary)
+    }
+    
     func startAdvertise(manager: CBPeripheralManager) {
         shouldAdvertise = true
         if manager.state == .poweredOn && !isAdvertising {
-            let primary = CBMutableService(type: service, primary: true)
-            let char = CBMutableCharacteristic(type: characteristic,
-                                               properties: CBCharacteristicProperties.read,
-                                               value: nil,
-                                               permissions: CBAttributePermissions.readable)
-            primary.characteristics = [char]
-            manager.add(primary)
-            print("starting advertising")
+            addCharacteristics(manager: manager)
+            log.debug("starting advertising")
             manager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [service]])
         }
     }
@@ -37,27 +46,21 @@ class Discoverable: NSObject, CBPeripheralManagerDelegate {
     func stopAdvertise(manager: CBPeripheralManager) {
         guard isAdvertising else { return }
         isAdvertising = false
-        print("stopping advertising")
+        log.debug("stopping advertising")
         manager.stopAdvertising()
     }
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         if peripheral.state == .poweredOn && shouldAdvertise && !isAdvertising {
-            let primary = CBMutableService(type: service, primary: true)
-            let char = CBMutableCharacteristic(type: characteristic,
-                                               properties: CBCharacteristicProperties.read,
-                                               value: nil,
-                                               permissions: CBAttributePermissions.readable)
-            primary.characteristics = [char]
-            peripheral.add(primary)
-            print("starting advertising")
+            addCharacteristics(manager: peripheral)
+            log.debug("starting advertising")
             peripheral.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [service]])
             isAdvertising = true
         }
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        print("didReceiveRead")
+        log.debug("didReceiveRead")
         request.value = SocketID.data
         peripheral.respond(to: request, withResult: .success)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
